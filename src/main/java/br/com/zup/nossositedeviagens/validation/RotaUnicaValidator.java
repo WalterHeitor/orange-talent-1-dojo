e60b0796bfd77805dbebdfb7d602e6505334d7af
Jonathas Lima <alexandrejonathas@gmail.com>
2021-02-05 17:52:21 -0300

    Jocimar

--- src/main/java/br/com/zup/nossositedeviagens/validation/RotaUnicaValidator.java
+++ src/main/java/br/com/zup/nossositedeviagens/validation/RotaUnicaValidator.java
@@ -1,11 +1,16 @@
 package br.com.zup.nossositedeviagens.validation;
 
 import br.com.zup.nossositedeviagens.rota.NovaRotaFormRequest;
+
+import org.springframework.util.Assert;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 public class RotaUnicaValidator implements Validator {
 
@@ -18,7 +23,12 @@ public class RotaUnicaValidator implements Validator {
     }
 
     @Override
-    public void validate(Object o, Errors errors) {
-
+    public void validate(Object target, Errors errors) {
+    	Query query = manager.createQuery("select 1 from Rota where aeroportoOrigem = :origem and aeroportoDestino = :destino");
+    	query.setParameter("origem", null);
+    	query.setParameter("destino", null);
+    	List<?> lista = query.getResultList();
+    	Assert.state(lista.size() <= 1, "Já existe uma rota com essa origem e destino");
+    	return lista.isEmpty();
     }
 }
