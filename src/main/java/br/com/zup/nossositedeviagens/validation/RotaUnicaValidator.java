c25077028021c242012d61863731542e32483eab
RebeccaMAriah <rebecca@prodev.com.br>
2021-02-26 16:44:05 -0300

    Rebecca e Lucas

--- src/main/java/br/com/zup/nossositedeviagens/validation/RotaUnicaValidator.java
+++ src/main/java/br/com/zup/nossositedeviagens/validation/RotaUnicaValidator.java
@@ -2,6 +2,7 @@ package br.com.zup.nossositedeviagens.validation;
 
 import br.com.zup.nossositedeviagens.rota.NovaRotaFormRequest;
 
+import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -11,7 +12,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-
+@Component
 public class RotaUnicaValidator implements Validator {
 
     @PersistenceContext
@@ -24,6 +25,9 @@ public class RotaUnicaValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
+        if (errors.hasErrors()){
+            return ;
+        }
     	Query query = manager.createQuery("select 1 from Rota where aeroportoOrigem.id = :origem and aeroportoDestino.id = :destino");
     	NovaRotaFormRequest request = (NovaRotaFormRequest) target;
     	query.setParameter("origem", request.getIdAeroportoOrigem());
@@ -31,8 +35,9 @@ public class RotaUnicaValidator implements Validator {
     	List<?> lista = query.getResultList();
     	Assert.state(lista.size() <= 1, "Já existe uma rota com essa origem e destino");
 
-    	if(lista.isEmpty()) {
-    	    return "A rota ou destino não pode ser vazia";
+    	if(!lista.isEmpty()) {
+    	    errors.rejectValue("origem_destino", null,
+                    "Não pode ser repetido os destinos ou origem");
         }
     }
 }
